9a5e1f2472fdd4e6ed9b80eba85c93b488591578
Basith <basithmus2021@gmail.com>
2024-06-10 19:20:27 +0530

    Changed link

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ const PORT = process.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
 
 setInterval(() => {
-    fetch('http://localhost:3000/reload').then(response => response.json()).then(data => console.log(data.message));
+    fetch('https://simple-node-app-nktc.onrender.com/reload').then(response => response.json()).then(data => console.log(data.message));
 }, 5000);
